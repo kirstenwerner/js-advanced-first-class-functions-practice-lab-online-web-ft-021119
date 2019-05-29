f7b6c6e84c68f57c5460cca9a8df4bc11ba3a0a8
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-29 01:23:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const nameCallback = function(element) {
+  console.log(element.name);
+};
+
+function logDriverNames(collection) {
+  collection.forEach(nameCallback);
+};
+
+function logDriversByHometown(collection, hometown) {
+  const driverHometownArray = collection.filter(driver => driver['hometown'] === hometown);
+  driverHometownArray.forEach(nameCallback);
+};
+
+function driversByRevenue(collection) {
+  const driversByRev = [...collection];
+  driversByRev.sort(function (a, b) {
+    return a.revenue - b.revenue;
+  })
+  return driversByRev;
+};
+
+function driversByName(collection) {
+  const sortedDrivers = [...collection];
+  return sortedDrivers.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+};
+
+function totalRevenue(collection) {
+  let rev = 0;
+  collection.forEach(function (collection) {
+    rev += collection.revenue;
+  });
+  return rev;
+}
+
+function averageRevenue(collection) {
+  return totalRevenue(collection) / collection.length
+}
